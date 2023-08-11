eb5ab1f8a4a7b8f7824a305408df0e5eb566df17
sdegnn <112622207+sdegnn@users.noreply.github.com>
2023-08-11 11:51:26 +0300

    modelling list method user with role

---
+++ Blog.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Blog.Entity.DTOs.Users;
+using Blog.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserController(UserManager<AppUser> userManager,IMapper mapper)
+        {
+            _userManager=userManager;
+            _mapper = mapper;
+            
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users=await _userManager.Users.ToListAsync();
+            var map = _mapper.Map<List<UserDto>>(users);
+            foreach(var item in map)
+            {
+                var findUser= await _userManager.FindByIdAsync(item.Id.ToString());
+                var role = string.Join("",await _userManager.GetRolesAsync(findUser));
+                item.Role= role;
+            }
+            
+
+                return View(map);
+
+        }
+
+    }
+}
